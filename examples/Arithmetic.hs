315ba57b1d1a5f5d7befd6e371d00ff9e2382c6b
Eric Paul <eric.1334@icloud.com>
2026-01-01 12:35:40 -0800

    Remove unnecessary imports and add example to hie.yaml

--- examples/Arithmetic.hs
+++ examples/Arithmetic.hs
@@ -1,7 +1,6 @@
-module Main where
+module Main (main) where
 
 import LeanParser
-import qualified Data.Map as Map
 
 command :: String
 command = "command"
@@ -19,5 +18,5 @@ main = do
     let leadingCtx = addLeadingParser command num emptyParsingRules
     let rulesCtx = addTrailingParsers command [plus, mul] leadingCtx
     let res = parse "12 * 3 + 65 * 4" rulesCtx
-    putStrLn (show res)
+    print res
     
